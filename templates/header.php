192c7fc330a0c29d184c81a53da30b3d7e13f7a9
Folletto <folletto@gmail.com>
2011-04-04 01:43:08 +0100

    Added navigation to test to-come automatic URL rewriter

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+  <meta charset="UTF-8"> 
+  <title><?php echo $title; ?></title>
+  
+  <!-- \/ Meta iOS -->
+  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  
+  <!-- \/ Template -->
+  <link rel="stylesheet" href="template.css" type="text/css" media="all" /> 
+  <link rel="apple-touch-icon" href="ios-icon-72x72.png"/>
+  <link rel="apple-touch-startup-image" href="ios-splash-320x460.png" />
+<head>
+<body>
---
+++ templates/menu.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="">Home</a></li>
+  <li><a href="about">About</a></li>
+  <li><a href="about/author">About the Author</a></li>
+</ul>
