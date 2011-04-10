34a419c7a87a47b625ceeafedab91bee80022752
Folletto <folletto@gmail.com>
2011-04-10 22:51:13 +0100

    Added license. Added a brief explanation.

--- Meek/Meek.php
+++ Meek/Meek.php
@@ -1,11 +1,68 @@
 <?php
 /*
  * Meek
- * last update: 2011-04-03
+ * last update: 2011-04-10
  * 
- * Copyright (C) 2010 by Davide S. Casali <folletto AT gmail DOT com>
- * Licensed under BSD License.
+ * ==========================================================================================
  * 
+ * BSD License:
+ * 
+ * Copyright (c) 2011 by Davide S. Casali <folletto AT gmail DOT com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are 
+ * permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this list of 
+ *   conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list of 
+ *   conditions and the following disclaimer in the documentation and/or other materials 
+ *   provided with the distribution.
+ * - Neither the name of "Meek" nor the names of its contributors may be used to 
+ *   endorse or promote products derived from this software without specific prior written 
+ *   permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * 
+ * ==========================================================================================
+ * 
+ * USAGE:
+ * Place the Meek folder in the root and add an 'index.php' file with it with one PHP line:
+ *   <?php require 'Meek/Meek.php'; ?>
+ * 
+ * A typical Meek app should have a folder structure like:
+ *   index.php
+ *   Meek/
+ *     Meek.php
+ *   pages/
+ *     404.php
+ *     index.php
+ *     some.sub.page.php
+ *   templates/
+ *     tpl.php
+ *     tpl.some.sub.php
+ * 
+ * Where:
+ * 'index.php'  contains just the include line <?php require 'Meek/Meek.php'; ?>.
+ * 'pages/'     files are the app pages, and have names that are going to match URLs:
+ *              'http://example.org/some/sub/page' will match 'pages/some.sub.page.php'
+ *              The only two exceptions are 'index.php' (the root) and '404.php' (file not found).
+ * 'templates/' contains the template, where only 'tpl.*.php' files are going to be read.
+ *              Templates' filenames will match, in a hierarchy, 'pages/' filenames:
+ *              'pages/some.sub.page.php' will load 'templates/tpl.some.sub.page.php'.
+ *              If the above is missing, the fallback template 'templates/tpl.some.sub.php' will be loaded.
+ *              If the above is missing too, then 'templates/tpl.some.php' will be loaded.
+ *              If the above is missing again, then 'templates/tpl.php' will be loaded.
+ *              'templates/tpl.php' is the top level template.
+ *              Standard PHP includes are allowed and you can add to the folder files
+ *              that doesn't begin with 'tpl.' to support them.
  */
 
 
